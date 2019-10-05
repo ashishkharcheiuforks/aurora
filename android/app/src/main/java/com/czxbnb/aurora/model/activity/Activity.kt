f4dee3bbd9d80e167c3e0bac88d6062e35a120bc
czxbnb <william@churinc.com>
2019-10-06 00:16:35 +1000

    activity data model

---
+++ android/app/src/main/java/com/czxbnb/aurora/model/activity/Activity.kt
@@ -0,0 +1,11 @@
+package com.czxbnb.aurora.model.activity
+
+data class Activity (
+    val id: String,
+    val title: String,
+    val content: String,
+    val time: String,
+    val duration: String,
+    val requirements: String,
+    val image: String
+)
